b285d714152fedd7f38c8ff183744149f6b54ae4
alex <aleksanderborovoydev@gmail.com>
2023-02-16 18:22:44 +0300

    Auto-generated commit [ci skip]

---
+++ assets/contract.sol
@@ -0,0 +1,55 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract NFTCollection is ERC721, ERC721URIStorage, Ownable {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIdCounter;
+
+    event CollectionCreated(
+        address indexed _contractAddress,
+        string _name,
+        string _symbol
+    );
+
+    event TokenMinted(
+        address indexed _contractAddress,
+        address _recipient,
+        uint256 _tokenId,
+        string _tokenUri
+    );
+
+    constructor(
+        string memory _name,
+        string memory _symbol
+    ) ERC721(_name, _symbol) {
+        emit CollectionCreated(address(this), _name, _symbol);
+    }
+
+    function mint(address to, string memory _tokenUri) public onlyOwner {
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, _tokenUri);
+        emit TokenMinted(address(this), to, tokenId, _tokenUri);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _burn(
+        uint256 tokenId
+    ) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(
+        uint256 tokenId
+    ) public view override(ERC721, ERC721URIStorage) returns (string memory) {
+        return super.tokenURI(tokenId);
+    }
+}
